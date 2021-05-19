41ef924eb1beac254aa84418ec889010fe92d93f
Ravi Kumar K G <ravi.rk2406@gmail.com>
2021-05-19 18:35:02 +0000

    initial commit

---
+++ src/main/java/com/covid19army/HelpRequestService/HelpRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.covid19army.HelpRequestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpRequestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid19army/HelpRequestService/HelpRequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid19army.HelpRequestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpRequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
